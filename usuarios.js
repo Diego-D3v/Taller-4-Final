a8af6eb23d4dce52d93e1141ac1e281e4988770d
HelenBarrosoS <hvgit198@gmail.com>
2025-10-01 20:08:37 -0500

    funciones de usuario

---
+++ usuarios.js
@@ -0,0 +1,30 @@
+// usuarios.js
+// Array inicial de usuarios para pruebas
+const usuarios = [
+  {  nombre: "William Donado", email: "william.donado@gmail.com" },
+  {  nombre: "Deiver Guerra", email: "deiver.guerra@gmail.com" },
+];
+ 
+// Función para registrar un nuevo usuario
+function registrarUsuario(nombre, email) {
+  const nuevoUsuario = {
+  
+    nombre,
+    email,
+  };
+  usuarios.push(nuevoUsuario);
+  console.log(`Usuario registrado: ${nombre}`);
+}
+ 
+// Función para listar todos los usuarios
+function listarUsuarios() {
+  console.log("Lista de usuarios:");
+  usuarios.forEach((u) => console.log(`${u.nombre} (${u.email})`));
+}
+ 
+// Exportar funciones y array usuarios
+module.exports = {
+  usuarios,
+  registrarUsuario,
+  listarUsuarios,
+};
