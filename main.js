20d65ed6bb0495881acbd8f5f7a38862e34d52f6
Diego <dadmdister@gmail.com>
2025-10-02 18:54:50 -0500

    adicion code prestamos y main

---
+++ main.js
@@ -0,0 +1,19 @@
+// main.js
+import { usuarios, registrarUsuario, listarUsuarios } from "./usuarios.js";
+import { libros, registrarLibro, listarLibros, listarLibrosDisponibles } from "./libros.js";
+import { prestarLibro, devolverLibro } from "./prestamos.js";
+
+// USUARIOS
+listarUsuarios();
+registrarUsuario("Carlos Gómez", "carlos.gomez@gmail.com");
+listarUsuarios();
+
+// LIBROS
+listarLibros();
+registrarLibro("La Sombra del Viento", "Carlos Ruiz Zafón");
+listarLibrosDisponibles();
+
+// PRÉSTAMOS
+prestarLibro("Cien años de soledad", "William Donado");
+prestarLibro("Don Quijote de la Mancha", "Deiver Guerra"); // ya estaba prestado
+devolverLibro("Don Quijote de la Mancha");
---
+++ readme.md
@@ -0,0 +1,7 @@
+# este es un programa que permite manejar el inventario de libros de una biblioteca funciona por modulos que son llamados atravez del archivo main.js
+**Miembros**
+William donado
+Diego Diaz
+Joan Rojas
+
+**se ejecuta atravez del achivo main**
