27796556e9013ff54c411960ad8b69e60ad3a9a7
sebastian <joansebastianrojassilva@gmail.com>
2025-10-01 20:50:22 -0500

    registro

---
+++ libros.js
@@ -0,0 +1,17 @@
+ const libros = [
+  { titulo: "Cien años de soledad", autor: "Gabriel García Márquez", estado: "disponible" },
+  { titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes", estado: "prestado" },
+  { titulo: "El Principito", autor: "Antoine de Saint-Exupéry", estado: "disponible" }
+];
+
+function Registrarlibro(titulo,autor,estado) {
+    libros.push({ titulo, autor, estado });
+    console.log(`Libro "${titulo}" registrado exitosamente.`);
+}
+
+Registrarlibro("La Sombra del Viento", "Carlos Ruiz Zafón", "disponible");
+
+function listarLibros() {
+
+}
+
